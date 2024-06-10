5bb05bd0301723072a301aff1064c036d8b782c0
ezekielswanson <swansonezekiel@gmail.com>
2024-06-09 20:51:50 -0500

    updated schema in movie.js

--- model/movie.js
+++ model/movie.js
@@ -2,11 +2,17 @@ const mongoose = require('mogoose');
 const Schema = mongoose.schema;
 
 
-
-
+//Schema
 const movieSchema = new Schema({
+    Poster: { type: String, required: true },
+    Title: { type: String, required: true },
+    imdbRating: { type: String, required: true },
+    Runtime: { type: String, required: true },
+    Genre: { type: String, required: true },
+    imdbID: { type: String, required: true, unique: true },
+    Plot: { type: String, required: true },
+  }, { timestamps: true });
 
 
 
-
-})
+  module.exports = mongoose.model('Movie', movieSchema);
